67ff9e39d8fd3babb6ef456a319a21a48f43de64
Shiraj Sayed <shirajsayed13@gmail.com>
2019-07-31 22:32:02 +0530

    Add splashActivity

--- app/src/main/java/com/shirajsayed/jsonplaceholder/MainActivity.java
+++ app/src/main/java/com/shirajsayed/jsonplaceholder/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Shiraj Sayed
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/shirajsayed/jsonplaceholder/SplashActivity.java
@@ -0,0 +1,7 @@
+package com.shirajsayed.jsonplaceholder;
+
+/**
+ * @author Shiraj Sayed
+ */
+public class SplashActivity {
+}
